8f9de6ed1243abf34e550d7f96d654372ad0a6f2
Viraj Warhade <86669937+virajwarhade@users.noreply.github.com>
2023-10-14 12:11:26 +0530

    header commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
+
 export default function Home() {
   return (
+    <>
+
     <div>Home</div>
+
+    </>
   )
 }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
+
 export default function Profile() {
   return (
+    <>
+    
     <div>Profile</div>
+    </>
   )
 }
