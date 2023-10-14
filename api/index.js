5ee5eaf896e66ddb7af6c4b4032891ed9c0e01a9
Viraj Warhade <86669937+virajwarhade@users.noreply.github.com>
2023-10-14 12:40:50 +0530

    db connect

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 import  express  from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then((data) => console.log("sucess conection " +data))
+.catch((err) => console.log("error conecting "+ err))
 
 const app = express()
 
